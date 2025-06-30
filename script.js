c4d7881c6d5309f274637822b75cfe115810a703
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-30 22:09:37 +0330

    change

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ plus.addEventListener("click", () => {
     num.innerText = a;
 })
 minus.addEventListener("click", () => {
-    if (a > 1) {
-        a--;
-        a = (a < 10) ? "0" + a : a;
-        num.innerText = a;
-    }
+   a--;
+   num.innerHTML=a;
 })
