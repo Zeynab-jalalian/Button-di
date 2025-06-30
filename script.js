c6a91f4ff9e763e76493f919de41a4e817abd75b
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-30 22:07:59 +0330

    finish

---
+++ script.js
@@ -0,0 +1,16 @@
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const num = document.querySelector(".num");
+let a = 1;
+plus.addEventListener("click", () => {
+    a++;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+})
+minus.addEventListener("click", () => {
+    if (a > 1) {
+        a--;
+        a = (a < 10) ? "0" + a : a;
+        num.innerText = a;
+    }
+})
